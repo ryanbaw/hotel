bae171bbd4c0ecedb59ca574f0dbaa68de76af26
libenhe <libenhe919@163.com>
2016-03-06 22:38:59 +0800

    订单详情接口

--- src/school/libenhe/hotel/dao/IOrderDetailDao.java
+++ src/school/libenhe/hotel/dao/IOrderDetailDao.java
@@ -1,5 +1,9 @@
 package school.libenhe.hotel.dao;
 
+import java.util.List;
+
+import school.libenhe.hotel.entity.OrderDetail;
+
 /**
  * 订单详情接口
  * 
@@ -8,4 +12,31 @@ package school.libenhe.hotel.dao;
  */
 public interface IOrderDetailDao {
 
+/**
+ * 增加
+ * 
+ * @author: Li Benhe Email: libenhe919@gmail.com
+ * @version: 2016-3-6 下午9:45:24 
+ * @param od
+ */
+void add(OrderDetail od);
+	
+	/**
+	 * 查询所有
+	 * 
+	 * @author: Li Benhe Email: libenhe919@gmail.com
+	 * @version: 2016-3-6 下午9:45:29 
+	 * @return
+	 */
+	List<OrderDetail> query();
+	
+	/**
+	 * 根据id查询
+	 * 
+	 * @author: Li Benhe Email: libenhe919@gmail.com
+	 * @version: 2016-3-6 下午9:45:36 
+	 * @param id
+	 * @return
+	 */
+	List<OrderDetail> findByOrderid(int id);
 }
