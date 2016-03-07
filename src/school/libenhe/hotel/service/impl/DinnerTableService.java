907e83353b05edfb9f3fa2648229156400e5c9a8
libenhe <libenhe919@163.com>
2016-03-07 22:02:23 +0800

    IDinnerTableService接口实现类

---
+++ src/school/libenhe/hotel/service/impl/DinnerTableService.java
@@ -0,0 +1,88 @@
+package school.libenhe.hotel.service.impl;
+
+import java.util.List;
+
+import school.libenhe.hotel.entity.DinnerTable;
+import school.libenhe.hotel.service.IDinnerTableService;
+
+/**
+ * IDinnerTableService接口实现类
+ * 
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @version 2016-3-7 下午5:59:08
+ */
+public class DinnerTableService implements IDinnerTableService{
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#add(school.libenhe.hotel.entity.DinnerTable)
+	 */
+	@Override
+	public void add(DinnerTable dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#delete(int)
+	 */
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#updata(school.libenhe.hotel.entity.DinnerTable)
+	 */
+	@Override
+	public void updata(DinnerTable dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#query()
+	 */
+	@Override
+	public List<DinnerTable> query() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#findById(int)
+	 */
+	@Override
+	public DinnerTable findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#query(java.lang.String)
+	 */
+	@Override
+	public List<DinnerTable> query(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#quitTable(int)
+	 */
+	@Override
+	public void quitTable(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see school.libenhe.hotel.service.IDinnerTableService#changesState(int)
+	 */
+	@Override
+	public DinnerTable changesState(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
