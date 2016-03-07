ac78937dca8e34b6eddae4a52c7c88b3ceaa37a5
libenhe <libenhe919@163.com>
2016-03-07 22:01:31 +0800

    订单详情Dao接口

--- src/school/libenhe/hotel/dao/IOrderDetailDao.java
+++ src/school/libenhe/hotel/dao/IOrderDetailDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import school.libenhe.hotel.entity.OrderDetail;
 
 /**
- * 订单详情接口
+ * 订单详情Dao接口
  * 
  * @author Li Benhe Email: libenhe919@gmail.com
  * @version 2016-3-1 下午2:43:17
