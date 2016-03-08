b303447eedba8ffbd603487e27d941ee6320439d
libenhe <libenhe919@163.com>
2016-03-08 22:30:31 +0800

    hotel update

---
+++ src/school/libenhe/hotel/servlet/BaseServlet.java
@@ -0,0 +1,35 @@
+package school.libenhe.hotel.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @version 2016-3-8 下午7:35:33
+ */
+public abstract class BaseServlet extends HttpServlet{
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+	
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
---
+++ src/school/libenhe/hotel/servlet/IndexServlet.java
@@ -0,0 +1,31 @@
+package school.libenhe.hotel.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @version 2016-3-8 下午6:50:37
+ */
+public class IndexServlet extends HttpServlet {
+
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+	}
+
+}
