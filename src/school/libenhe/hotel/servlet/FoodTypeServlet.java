5ef4cd9fe35c003a110281fa0f6176ca0f73940b
libenhe <libenhe919@163.com>
2016-03-09 22:18:29 +0800

    FoodTypeServlet

--- src/school/libenhe/hotel/servlet/FoodTypeServlet.java
+++ src/school/libenhe/hotel/servlet/FoodTypeServlet.java
@@ -1,12 +1,21 @@
 package school.libenhe.hotel.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import school.libenhe.hotel.entity.FoodType;
+import school.libenhe.hotel.factory.BeanFactory;
+import school.libenhe.hotel.service.IFoodTypeService;
+import school.libenhe.hotel.utils.WebUtils;
 
 /**
  * @author Li Benhe Email: libenhe919@gmail.com
@@ -14,60 +23,140 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class FoodTypeServlet extends HttpServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+	//创建service
+	private IFoodTypeService service = BeanFactory.getInstance("foodTypeService", IFoodTypeService.class);
+	
+	private Object uri;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		List<FoodType> list = service.query();
+		config.getServletContext().setAttribute("foodtype", list);
+	}
+	
+	
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+	String method = request.getParameter("method");
+		
+		if("add".equals(method)){
+			add(request,response);
+		}else if("list".equals(method)){
+			list(request,response);
+		}else if("update".equals(method)){
+			update(request,response);
+		}else if("delete".equals(method)){
+			delete(request,response);
+		}else if("search".equals(method)){
+			search(request,response);
+		}else if("show".equals(method)){
+			show(request, response);
+		}
+		
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+	private void add(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String name = request.getParameter("name");
+			FoodType foodtype = new FoodType();
+			foodtype.setTypeName(name);
+			service.add(foodtype);
+			list(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			uri="/error/error.jsp";
+		}
+	}
+	
+	private void list(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			List<FoodType> list = service.query();
+			request.setAttribute("list", list);
+			request.getServletContext().setAttribute("foodtype", list);
+			uri = request.getRequestDispatcher("/sys/foodtype/cuisineList.jsp");
+		} catch (Exception e) {
+			e.printStackTrace();
+			uri="/error/error.jsp";
+		}finally {
+			WebUtils.goTo(request, response, uri);
+		}
+	}
+	
+
+	//在菜系更新中显示菜系对应的类型的名称
+		private void show(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			try {
+				
+				String id = request.getParameter("id");
+				FoodType type = service.findById(Integer.parseInt(id));
+				request.setAttribute("type", type);
+				uri = request.getRequestDispatcher("/sys/foodtype/updateCuisine.jsp");
+			} catch (Exception e) {
+				e.printStackTrace();
+				uri="/error/error.jsp";
+			}finally {
+				WebUtils.goTo(request, response, uri);
+			}
+		}
+	
+	
+		private void update(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			try {
+				FoodType type = new FoodType();
+				Map<String, String[]> map = request.getParameterMap();
+				BeanUtils.populate(type, map);
+				service.updata(type);
+				list(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+				uri="/error/error.jsp";
+			}
+		}
+		
+		
+		private void delete(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			try {
+				String id = request.getParameter("id");
+				service.delete(Integer.parseInt(id));
+				list(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+				uri="/error/error.jsp";
+				WebUtils.goTo(request, response, uri);
+			}
+		}
+		
+		
+		
+		private void search(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			try {
+				String keyword = request.getParameter("keyword");
+				if(keyword!=null){
+					List<FoodType> list = service.query(keyword);
+					request.setAttribute("list",list);
+					uri=request.getRequestDispatcher("/sys/foodtype/cuisineList.jsp");
+				}
+			} catch (Exception e) {
+				uri="/error/error.jsp";
+				e.printStackTrace();
+			}
+			WebUtils.goTo(request, response, uri);
+		}
+		
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the POST method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+	   this.doGet(request, response);
+		
 	}
 
 }
